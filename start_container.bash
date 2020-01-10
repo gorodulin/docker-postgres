0a439be244762a2400522fc0d58d7e1d652003b3
Vladimir Gorodulin <ru.hostmaster@gmail.com>
2020-01-10 17:04:13 +0000

    Added superuser name variable

--- start_container.bash
+++ start_container.bash
@@ -11,6 +11,7 @@ exe docker run \
   --rm \
   --detach \
   -p $PG_EXPOSED_PORT:5432 \
+  -e POSTGRES_USER=$PG_CUSTOM_SUPERUSER_NAME \
   -e POSTGRES_PASSWORD=$PG_CUSTOM_SUPERUSER_PASSWORD \
   --name=$PG_CONTAINER_NAME \
   --hostname=$PG_CONTAINER_HOSTNAME \
